e74367d5ceed4c1a22abd74e2e80c122bf305ae7
Juliocrmks <57975456+Juliocrmks@users.noreply.github.com>
2021-10-06 23:03:52 -0500

    Add files via upload

---
+++ Prototipo/Prototipo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Prototipo
+//
+//  Created by Blue People on 06/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Image("img3").resizable()
+    }
+    
+    
+}
+
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Prototipo/Prototipo/PrototipoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrototipoApp.swift
+//  Prototipo
+//
+//  Created by Blue People on 06/10/21.
+//
+
+import SwiftUI
+
+@main
+struct PrototipoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
